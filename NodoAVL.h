c8cc5a641274dc376a7fa7bed42a3b8d2d43408d
Jere348 <120813633+Jere348@users.noreply.github.com>
2025-06-14 15:25:24 -0500

    Add files via upload
    Implementación de arboles con usuario y historial de productos

---
+++ NodoAVL.h
@@ -0,0 +1,15 @@
+#pragma once
+// Nodo para árbol AVL genérico
+
+template <typename T>
+class NodoAVL {
+public:
+    T* dato;
+    NodoAVL* izq;
+    NodoAVL* der;
+    int altura;
+
+    NodoAVL(T* pdato)
+        : dato(pdato), izq(nullptr), der(nullptr), altura(1) {
+    }
+};
